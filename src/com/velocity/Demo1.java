f9b9ae5bf9b280ce3f7d4ae7f499f4168651067a
devendra <1997devendrabhopale@gmail.com>
2023-01-17 22:14:17 +0530

    array

---
+++ src/com/velocity/Demo1.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>(); 
+
+		al.add("java");
+		al.add("spring");
+		al.add("collection");
+		
+		
+		System.out.println(al);
+	}
+
+}
